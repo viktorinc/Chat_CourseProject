7341f178a2d91237d7a59ef6ad3dd739ad5966f0
viktor <viktorkubrakpl@gmail.com>
2019-06-10 23:19:16 +0300

    Add project files.

---
+++ Chat_Course_Project/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chat_Course_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string User_Name="Anonymous"; // ім`я користувача, по дефолту - анонім
+        public MainWindow()
+        {
+            Window1 win = new Window1();
+            win.Show();
+            System.Windows.Threading.DispatcherTimer DesTimer = new System.Windows.Threading.DispatcherTimer(); // таймер
+            DesTimer.Tick += new EventHandler(DesTimer_Tick);
+            DesTimer.Interval = new TimeSpan(0, 0, 1);
+            DesTimer.Start();
+        }
+
+        private void Send_Btn_Clicked(object sender, RoutedEventArgs e)// Записує чат в файл, з добавленням імені, якшо імені не вказано, наш юзр - анонім
+        {
+            Main_Chat.Content += $"\n{User_Name}: {Send_Box.Text}";
+            string chat =(string)Main_Chat.Content;
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("../../Chat_Content.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, chat);
+                fs.Close();
+            }
+            Send_Box.Text = "";
+
+        }
+        private void DesTimer_Tick(object sender, EventArgs e) // тік таймера, кожну секунду десереалізує файл в scrollviewer
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                using (FileStream fs = new FileStream("../../Chat_Content.dat", FileMode.OpenOrCreate))
+                {
+                    Main_Chat.Content = (string)formatter.Deserialize(fs);
+                    fs.Close();
+                }
+            }
+            catch { }
+        }
+    }
+
+}
---
+++ Chat_Course_Project/Window1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Shapes;
+
+namespace Chat_Course_Project
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        Dictionary<string, string> dict;
+        public Window1()
+        {
+            
+        }
+        private void Log_In_Btn(object sender, RoutedEventArgs e)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                using (FileStream fs = new FileStream("../../User_Base.dat", FileMode.OpenOrCreate))
+                {
+                    dict = (Dictionary<string, string>)formatter.Deserialize(fs);
+                    fs.Close();
+                }
+                foreach (var el in dict)
+                {
+                    if (el.Key==username && el.Value==password)
+                    {
+                        this.Close();
+                    }
+                }
+                 MessageBox.Show("Error", "Incorrect login or password"); 
+            }
+            catch { }
+        }
+        private void Reg_Btn(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
